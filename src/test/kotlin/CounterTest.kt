d1d5da57c47c8d445a622fd9daf1ae2b4285f353
mvicsokolova <maria.sokolova@jetbrains.com>
2021-06-13 00:33:58 +0300

    1.1-counter

---
+++ src/test/kotlin/CounterTest.kt
@@ -0,0 +1,18 @@
+import org.jetbrains.kotlinx.lincheck.*
+import org.jetbrains.kotlinx.lincheck.annotations.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.*
+import org.jetbrains.kotlinx.lincheck.strategy.stress.*
+import org.junit.*
+
+class CounterTest {
+    private val c = Counter() // the initial state
+
+    // Operations on the Counter
+    @Operation
+    fun addAndGet(delta: Int) = c.addAndGet(delta)
+    @Operation
+    fun get() = c.get()
+
+    @Test // stress test
+    fun test() = StressOptions().check(this::class)
+}
