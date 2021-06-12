fe6a446795daa3a64b42452edb545bccdbf77c7c
mvicsokolova <maria.sokolova@jetbrains.com>
2021-06-13 01:30:25 +0300

    5.1-dataholder-solution

---
+++ src/test/kotlin/DataHolderTest.kt
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlinx.lincheck.*
+import org.jetbrains.kotlinx.lincheck.annotations.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.*
+import org.junit.*
+
+@OpGroupConfig(name = "writer", nonParallel = true)
+class DataHolderTest {
+    private val dataHolder = DataHolder()
+
+    @Operation(group = "writer")
+    fun write(first: Long, second: Long) = dataHolder.write(first, second)
+
+    @Operation
+    fun read() = dataHolder.read()
+
+    @Test
+    fun runModelCheckingTest() = ModelCheckingOptions()
+        .checkObstructionFreedom()
+        .check(this::class.java)
+}
