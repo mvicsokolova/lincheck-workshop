d0aef5f969b29e4257055d4f39842b2f93a924ca
mvicsokolova <maria.sokolova@jetbrains.com>
2021-06-13 01:52:40 +0300

    8.1-rendezvous-channel

---
+++ src/test/kotlin/RendezvousChannelTest.kt
@@ -0,0 +1,28 @@
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.channels.ClosedSendChannelException
+import org.jetbrains.kotlinx.lincheck.LoggingLevel
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.annotations.Param
+import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
+import org.junit.Test
+
+@Param(name = "value", gen = IntGen::class, conf = "1:5")
+class RendezvousChannelTest {
+    private val ch = Channel<Int>()
+
+    @Operation
+    suspend fun send(@Param(name = "value") value: Int) = ch.send(value)
+
+    @Operation
+    suspend fun receive() = ch.receive()
+
+    @Test
+    fun runStressTest() = StressOptions().check(this::class)
+
+    @Test
+    fun runModelCheckingTest() = ModelCheckingOptions().check(this::class)
+}
