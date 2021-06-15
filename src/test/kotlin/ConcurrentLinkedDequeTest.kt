8c8d2457a0ceb695d1e22992312fb08e759128e3
Nikita Koval <nikita.koval@jetbrains.com>
2021-06-15 18:00:46 +0600

    The final task solution

--- src/test/kotlin/ConcurrentLinkedDequeTest.kt
+++ src/test/kotlin/ConcurrentLinkedDequeTest.kt
@@ -1,2 +1,34 @@
-class ConcurrentLinkedDequeTest {
+import org.jetbrains.kotlinx.lincheck.*
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.strategy.stress.*
+import org.jetbrains.kotlinx.lincheck.verifier.*
+import org.junit.*
+import java.util.concurrent.*
+
+class ConcurrentLinkedDequeTest : VerifierState() {
+    private val deque = ConcurrentLinkedDeque<Int>()
+
+    @Operation
+    fun addFirst(value: Int) = deque.addFirst(value)
+
+    @Operation
+    fun addLast(value: Int) = deque.addLast(value)
+
+    @Operation
+    fun getFirst() = deque.peekFirst()
+
+    @Operation // this operation is non-linearizable!
+    fun getLast() = deque.peekLast()
+
+    @Operation
+    fun pollFirst() = deque.pollFirst()
+
+    @Operation
+    fun pollLast() = deque.pollLast()
+
+    override fun extractState() = deque.toList()
+
+    @Test
+    fun runModelCheckingTest() = StressOptions()
+        .check(ConcurrentLinkedDequeTest::class)
 }
